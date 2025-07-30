a3a9da004529b6f73cad6ba5ed2dcc0ef917f508
fcdmoraes <ffllaa@gmail.com>
2025-07-30 15:05:15 -0300

    ajuste classe Produto: adicionado id único

--- Desafio 1/src/Produto.java
+++ Desafio 1/src/Produto.java
@@ -1,15 +1,20 @@
 public class Produto {
+    private static int produtos = 0;
+
+    private final int itemId;
     private int estoque;
-    private String nome;
+    private final String nome;
     private double preco;
 
     public Produto(String nome, double preco, int estoque){
+        this.itemId = produtos++;
         this.nome = nome;
         this.preco = preco;
         this.estoque = estoque;
     }
 
     public Produto(String nome, int preco){
+        this.itemId = produtos++;
         this.nome = nome;
         this.preco = preco;
         this.estoque = 0;
@@ -20,14 +25,50 @@ public class Produto {
     }
 
     public void vender(int quantidade){
-        if(this.estoque > quantidade){
-            this.estoque -= quantidade;
+        if(estoque > quantidade){
+            estoque -= quantidade;
+        }
+        else{
+            System.out.println("Estoque insuficiente");
         }
     }
 
     public void exibirResumo() {
-        System.out.println("Nome: " + this.nome);
-        System.out.println("Preço: " + this.preco);
-        System.out.println("Estoque : " + this.estoque);
+        System.out.println("Id: " + itemId);
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: R$ " + preco);
+        System.out.println("Estoque: " + estoque);
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void ajustarPreco(double novoPreco) {
+        preco = novoPreco;
+    }
+
+    public void ajustarPreco(String reajuste) {
+        try {
+            String percentual = reajuste.trim()
+                    .replace("%", "")
+                    .replace(",", ".");
+            preco = preco * (1 + Double.parseDouble(percentual) / 100);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Formato inválido");
+        }
     }
 }
